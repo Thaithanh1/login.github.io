db8e7beb2bbcd352b7454ee5563c2964bea7f0ed
Thaithanh1 <77682579+Thaithanh1@users.noreply.github.com>
2021-04-02 16:55:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ span.psw {
 <body>
 	<form method="post">
 	  <div class="imgcontainer">
-	    <img url="https://miro.medium.com/max/400/1*7c5wdOe8QXTgw_tfUi11rA.png" alt="Avatar" class="avatar">
+	    <img src="https://miro.medium.com/max/400/1*7c5wdOe8QXTgw_tfUi11rA.png" alt="Avatar" class="avatar">
 	  </div>
 
 	  <div class="container">
